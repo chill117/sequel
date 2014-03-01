aaaa8abef68a29118178bc3e0802433cfda80d23
Charles Hill <charlesh@degreesofzero.com>
2014-03-01 22:59:16 +0100

    Cleaned up the Instance#hasChanged tests a bit; mostly made each test independent

--- test/unit/instance/hasChanged.js
+++ test/unit/instance/hasChanged.js
@@ -11,7 +11,7 @@ describe('Instance#hasChanged(name)', function() {
 	before(TestManager.setUp)
 	after(TestManager.tearDown)
 
-	var model, instance, data, newData
+	var model, data1, data2
 
 	before(function() {
 
@@ -22,12 +22,6 @@ describe('Instance#hasChanged(name)', function() {
 			an_array_of_integers: 'array-integer',
 			an_array_of_strings: 'array-string',
 			an_array_of_floats: 'array-float',
-			an_empty_text_array: 'array-text',
-			an_empty_number_array: 'array-number',
-			a_read_only_array: {
-				type: 'array-text',
-				readOnly: true
-			},
 			a_decimal: 'decimal',
 			an_integer: 'integer',
 			a_date: 'date'
@@ -39,34 +33,19 @@ describe('Instance#hasChanged(name)', function() {
 
 		})
 
-		data = {
+		data1 = {
 			a_string: 'a regular amount of text',
 			a_long_string: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue.',
 			an_array_of_integers: [1, 2, 3],
 			an_array_of_strings: ['an', 'array', 'of', 'strings'],
 			an_array_of_floats: [15.01, 20, 300.001],
 			an_array_of_dates: [new Date(), new Date()],
-			an_empty_text_array: [],
-			an_empty_number_array: [],
 			a_decimal: 201.54,
 			an_integer: 15,
 			a_date: new Date()
 		}
 
-		instance = model.build(data)
-
-	})
-
-	it('should return FALSE for fields that have not been changed', function() {
-
-		for (var field in data)
-			expect(instance.hasChanged(field)).to.equal(false)
-
-	})
-
-	it('should return TRUE for fields that have been changed', function() {
-
-		newData = {
+		data2 = {
 			a_string: 'a different string',
 			a_long_string: 'Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.',
 			an_array_of_integers: [4, 5, 6],
@@ -78,14 +57,33 @@ describe('Instance#hasChanged(name)', function() {
 			a_date: new Date(new Date().getTime() - 15000)
 		}
 
-		instance.set(newData)
+	})
 
-		for (var field in newData)
+	it('should return FALSE for fields that have not been changed', function() {
+
+		var instance = model.build(data1)
+
+		for (var field in data1)
+			expect(instance.hasChanged(field)).to.equal(false)
+
+	})
+
+	it('should return TRUE for fields that have been changed, before the changes are saved to the database', function() {
+
+		var instance = model.build(data1)
+
+		instance.set(data2)
+
+		for (var field in data2)
 			expect(instance.hasChanged(field)).to.equal(true)
 
 	})
 
-	it('should return FALSE for all the changed fields, after the changes are saved to the database', function(done) {
+	it('should return FALSE for all the fields that were changed, after the changes are saved to the database', function(done) {
+
+		var instance = model.build(data1)
+
+		instance.set(data2)
 
 		instance.save().complete(function(errors, result) {
 
@@ -95,7 +93,7 @@ describe('Instance#hasChanged(name)', function() {
 				return done(new Error('An unexpected error has occurred'))
 			}
 
-			for (var field in newData)
+			for (var field in data2)
 				expect(result.hasChanged(field)).to.equal(false)
 
 			done()
@@ -104,28 +102,85 @@ describe('Instance#hasChanged(name)', function() {
 
 	})
 
-	it('should return TRUE for an array field that had its previous values overwritten', function() {
+	it('for an instance that had data changed and then saved to the database, should return TRUE for fields that are changed back to their original values', function(done) {
 
-		var an_array_of_strings = instance.get('an_array_of_strings')
+		var instance = model.build(data1)
 
-		for (var i in an_array_of_strings)
-			an_array_of_strings[i] = 'changed'
+		instance.set(data2)
 
-		instance.set('an_array_of_strings', an_array_of_strings)
+		instance.save().complete(function(errors, result) {
 
-		expect(instance.hasChanged('an_array_of_strings')).to.equal(true)
+			if (errors)
+			{
+				console.log(errors)
+				return done(new Error('An unexpected error has occurred'))
+			}
+
+			instance.set(data1)
+
+			for (var field in data1)
+				expect(instance.hasChanged(field)).to.equal(true)
+
+			done()
+
+		})
 
 	})
 
-	it('should return TRUE for an array field that was emptied', function() {
+	it('should return TRUE for an array field that had its previous values overwritten', function(done) {
 
-		var an_array_of_integers = instance.get('an_array_of_integers')
+		var instance = model.build(data1)
 
-		an_array_of_integers = []
+		instance.set(data2)
 
-		instance.set('an_array_of_integers', an_array_of_integers)
+		instance.save().complete(function(errors, result) {
 
-		expect(instance.hasChanged('an_array_of_integers')).to.equal(true)
+			if (errors)
+			{
+				console.log(errors)
+				return done(new Error('An unexpected error has occurred'))
+			}
+
+			var an_array_of_strings = instance.get('an_array_of_strings')
+
+			for (var i in an_array_of_strings)
+				an_array_of_strings[i] = 'changed'
+
+			instance.set('an_array_of_strings', an_array_of_strings)
+
+			expect(instance.hasChanged('an_array_of_strings')).to.equal(true)
+
+			done()
+
+		})
+
+	})
+
+	it('should return TRUE for an array field that was emptied', function(done) {
+
+		var instance = model.build(data1)
+
+		instance.set(data2)
+
+		instance.save().complete(function(errors, result) {
+
+			if (errors)
+			{
+				console.log(errors)
+				return done(new Error('An unexpected error has occurred'))
+			}
+
+			var an_array_of_integers = instance.get('an_array_of_integers')
+
+			an_array_of_integers = []
+
+			instance.set('an_array_of_integers', an_array_of_integers)
+
+			expect(instance.hasChanged('an_array_of_integers')).to.equal(true)
+
+			done()
+
+		})
 
 	})
 
