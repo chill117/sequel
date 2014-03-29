bc1fe781a1278b44720294224121ea9a42a2d5df
Charles Hill <charlesh@degreesofzero.com>
2014-03-29 15:34:43 +0100

    Updated dependencies
    Added .travis.yml file
    
    Added to .npmignore file
    
    Set support node version to >=0.8.0

--- test/config/database.js
+++ test/config/database.js
@@ -1,10 +1,10 @@
 module.exports = {
 	mysql: {
-		host: 'localhost',
-		port: 3306,
-		user: 'sequel_test',
-		password: 'password',
-		database: 'sequel_test',
+		host: process.env.DB_HOST !== undefined ? process.env.DB_HOST : 'localhost',
+		port: process.env.DB_PORT !== undefined ? process.env.DB_PORT : 3306,
+		user: process.env.DB_USER !== undefined ? process.env.DB_USER : 'sequel_test',
+		password: process.env.DB_PASS !== undefined ? process.env.DB_PASS : 'password',
+		database: process.env.DB_NAME !== undefined ? process.env.DB_NAME : 'sequel_test',
 		driver: 'mysql'
 	},
 	sqlite: {
