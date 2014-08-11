374d396df6a1803e854aa8ca0f8b9b1536ed3b9a
Charles Hill <chill@degreesofzero.com>
2014-08-11 23:03:50 +0200

    Refactoring for validation unit tests, precision validation should ignore non-numbers.

--- test/unit/validation.js
+++ test/unit/validation.js
@@ -1,221 +1,300 @@
 var expect = require('chai').expect
+var Validation = require('../drivers/mysql/sequel').validation
 
 
 describe('Validation', function() {
 
-	var Validation
+	describe('notEmpty', function() {
 
-	before(function() {
+		it('should exist', function() {
 
-		Validation = sequel.validation
+			expect(Validation.testExists('notEmpty')).to.equal(true)
+
+		})
+
+		it('should pass', function() {
+
+			expect(Validation.test('notEmpty', ' hi')).to.equal(true)
+			expect(Validation.test('notEmpty', 'there !')).to.equal(true)
+			expect(Validation.test('notEmpty', "\n with a linebreak")).to.equal(true)
+
+		})
+
+		it('should fail', function() {
+
+			expect(Validation.test('notEmpty', ' ')).to.equal(false)
+			expect(Validation.test('notEmpty', '   ')).to.equal(false)
+			expect(Validation.test('notEmpty', "\n ")).to.equal(false)
+			expect(Validation.test('notEmpty', "\t ")).to.equal(false)
+
+		})
 
 	})
 
-	describe('#customFunctions', function() {
+	describe('notNull', function() {
 
-		var customFunctions = {
+		it('should exist', function() {
 
-			notEmpty: [
-				{
-					pass: [
-						' hi',
-						'there !',
-						"\n with a linebreak"
-					],
-					fail: [
-						" ",
-						"\n ",
-						" \t",
-						" \t "
-					]
-				}
-			],
+			expect(Validation.testExists('notNull')).to.equal(true)
 
-			notNull: [
-				{
-					pass: [
-						0,
-						200,
-						300.22,
-						new Date(),
-						'hi!'
-					],
-					fail: [
-						null,
-						''
-					]
-				}
-			],
+		})
 
-			notIn: [
-				{
-					args: [1, 2, 3, 4, 6],
-					pass: [ 0, 5, 200 ],
-					fail: [ 1, 2, 3, 6 ]
-				},
-				{
-					args: ['hi', 'there', 'friend'],
-					pass: [ 1, 2, 3, 6, 'howdy' ],
-					fail: [ 'hi', 'there', 'friend' ]
-				}
-			],
+		it('should pass', function() {
 
-			isIn: [
-				{
-					args: [1, 2, 3, 4, 6],
-					pass: [ 1, 2, 3, 6 ],
-					fail: [ 0, 5, 200 ]
-				},
-				{
-					args: ['hi', 'there', 'friend'],
-					pass: [ 'hi', 'there', 'friend' ],
-					fail: [ 1, 2, 3, 6, 'howdy' ]
-				}
-			],
+			expect(Validation.test('notNull', 0)).to.equal(true)
+			expect(Validation.test('notNull', 200)).to.equal(true)
+			expect(Validation.test('notNull', new Date())).to.equal(true)
+			expect(Validation.test('notNull', 'hi!')).to.equal(true)
+			expect(Validation.test('notNull', ' ')).to.equal(true)
 
-			isDecimal: [
-				{
-					pass: [ 0.12, 15.1, 200.00002 ],
-					fail: [ 1, 2, 3, 6, '', 'hi', undefined, null, true, false ]
-				}
-			],
+		})
 
-			isNumber: [
-				{
-					pass: [ 1, 2, 3, 6, 0.12, 15.1, 200.00002 ],
-					fail: [ '', 'hi', undefined, null, true, false ]
-				}
-			],
+		it('should fail', function() {
 
-			min: [
-				{
-					args: 10,
-					pass: [ 12, 26, 200 ],
-					fail: [ 0, 5, 9 ]
-				}
-			],
+			expect(Validation.test('notNull', '')).to.equal(false)
+			expect(Validation.test('notNull', null)).to.equal(false)
 
-			max: [
-				{
-					args: 10,
-					pass: [ 0, 5, 9 ],
-					fail: [ 12, 26, 200 ]
-				}
-			],
+		})
 
-			minLen: [
-				{
-					args: 8,
-					pass: [
-						'this should be long enough',
-						'password',
-						'testing!',
-						' testing',
-						'testing '
-					],
-					fail: [
-						'almost!',
-						'nope',
-						' nope',
-						'nope ',
-						'',
-						' '
-					]
-				}
-			],
+	})
 
-			maxLen: [
-				{
-					args: 8,
-					pass: [
-						'almost!',
-						'nope',
-						' nope',
-						'nope ',
-						'',
-						' '
-					],
-					fail: [
-						'this should be too long',
-						' password ',
-						'! testing! ',
-						' #testing ',
-						'09 testing  '
-					]
-				}
-			],
+	describe('notIn', function() {
 
-			precision: [
-				{
-					args: 4,
-					pass: [ 10, 0.12, 15.1, 200.0002 ],
-					fail: [ 1.00001, 1000.05001, 0.00003333333 ]
-				}
-			]
+		it('should exist', function() {
 
-		}
+			expect(Validation.testExists('notIn')).to.equal(true)
 
-		for (var fn in customFunctions)
-			it('\'' + fn + '\' should exist', function() {
+		})
 
-				expect(Validation.testExists(fn)).to.equal(true)
+		it('should pass', function() {
 
-			})
+			expect(Validation.test('notIn', 0, [1, 2, 3, 4, 6])).to.equal(true)
+			expect(Validation.test('notIn', 'hi', [1, 2, 3, 4, 6])).to.equal(true)
+			expect(Validation.test('notIn', ' ', ['hi', 'there', 'friend'])).to.equal(true)
+			expect(Validation.test('notIn', 2, ['hi', 'there', 'friend'])).to.equal(true)
 
-		for (var fn in customFunctions)
-			it('\'' + fn + '\' should pass as expected', function() {
+		})
 
-				var testCases = customFunctions[fn]
+		it('should fail', function() {
 
-				for (var i in testCases)
-				{
-					var testCase = testCases[i]
-					var args = testCase.args || []
-					var pass = testCase.pass
+			expect(Validation.test('notIn', 4, [1, 2, 3, 4, 6])).to.equal(false)
+			expect(Validation.test('notIn', 3, [1, 2, 3, 4, 6])).to.equal(false)
+			expect(Validation.test('notIn', 'hi', ['hi', 'there', 'friend'])).to.equal(false)
+			expect(Validation.test('notIn', 'there', ['hi', 'there', 'friend'])).to.equal(false)
 
-					for (var i in pass)
-					{
-						var value = pass[i]
+		})
 
-						var result = Validation.test(fn, value, args)
+	})
 
-						expect(result).to.equal(true)
-					}
-				}
+	describe('isIn', function() {
 
-			})
+		it('should exist', function() {
 
-		for (var fn in customFunctions)
-			it('\'' + fn + '\' should fail as expected', function() {
+			expect(Validation.testExists('isIn')).to.equal(true)
 
-				var testCases = customFunctions[fn]
+		})
 
-				for (var i in testCases)
-				{
-					var testCase = testCases[i]
-					var args = testCase.args || []
-					var fail = testCase.fail
+		it('should pass', function() {
 
-					for (var i in fail)
-					{
-						var value = fail[i]
-						var result = Validation.test(fn, value, args)
+			expect(Validation.test('isIn', 4, [1, 2, 3, 4, 6])).to.equal(true)
+			expect(Validation.test('isIn', 3, [1, 2, 3, 4, 6])).to.equal(true)
+			expect(Validation.test('isIn', 'hi', ['hi', 'there', 'friend'])).to.equal(true)
+			expect(Validation.test('isIn', 'there', ['hi', 'there', 'friend'])).to.equal(true)
 
-						expect(result).to.equal(false)
-					}
-				}
+		})
 
-			})
+		it('should fail', function() {
 
-		for (var fn in customFunctions)
-			it('\'' + fn + '\' should have a default error message', function() {
+			expect(Validation.test('isIn', 0, [1, 2, 3, 4, 6])).to.equal(false)
+			expect(Validation.test('isIn', 'hi', [1, 2, 3, 4, 6])).to.equal(false)
+			expect(Validation.test('isIn', ' ', ['hi', 'there', 'friend'])).to.equal(false)
+			expect(Validation.test('isIn', 2, ['hi', 'there', 'friend'])).to.equal(false)
 
-				var errorMessage = Validation.getError(fn)
+		})
 
-				expect(errorMessage).to.be.a('string')
-				expect(errorMessage.length).to.not.equal(0)
+	})
 
-			})
+	describe('isDecimal', function() {
+
+		it('should exist', function() {
+
+			expect(Validation.testExists('isDecimal')).to.equal(true)
+
+		})
+
+		it('should pass', function() {
+
+			expect(Validation.test('isDecimal', 0.1)).to.equal(true)
+			expect(Validation.test('isDecimal', 0.102)).to.equal(true)
+			expect(Validation.test('isDecimal', 100.10)).to.equal(true)
+
+		})
+
+		it('should fail', function() {
+
+			expect(Validation.test('isDecimal', '')).to.equal(false)
+			expect(Validation.test('isDecimal', 'hi')).to.equal(false)
+			expect(Validation.test('isDecimal', null)).to.equal(false)
+			expect(Validation.test('isDecimal', false)).to.equal(false)
+			expect(Validation.test('isDecimal', undefined)).to.equal(false)
+			expect(Validation.test('isDecimal', true)).to.equal(false)
+
+		})
+
+	})
+
+	describe('isNumber', function() {
+
+		it('should exist', function() {
+
+			expect(Validation.testExists('isNumber')).to.equal(true)
+
+		})
+
+		it('should pass', function() {
+
+			expect(Validation.test('isNumber', 0.1)).to.equal(true)
+			expect(Validation.test('isNumber', 0.102)).to.equal(true)
+			expect(Validation.test('isNumber', 0)).to.equal(true)
+			expect(Validation.test('isNumber', 22)).to.equal(true)
+
+		})
+
+		it('should fail', function() {
+
+			expect(Validation.test('isNumber', '')).to.equal(false)
+			expect(Validation.test('isNumber', 'hi')).to.equal(false)
+			expect(Validation.test('isNumber', "\n")).to.equal(false)
+			expect(Validation.test('isNumber', null)).to.equal(false)
+			expect(Validation.test('isNumber', false)).to.equal(false)
+			expect(Validation.test('isNumber', undefined)).to.equal(false)
+			expect(Validation.test('isNumber', true)).to.equal(false)
+
+		})
+
+	})
+
+	describe('min', function() {
+
+		it('should exist', function() {
+
+			expect(Validation.testExists('min')).to.equal(true)
+
+		})
+
+		it('should pass', function() {
+
+			expect(Validation.test('min', 10, 10)).to.equal(true)
+			expect(Validation.test('min', 200, 10)).to.equal(true)
+
+		})
+
+		it('should fail', function() {
+
+			expect(Validation.test('min', 0, 10)).to.equal(false)
+			expect(Validation.test('min', 9, 10)).to.equal(false)
+
+		})
+
+	})
+
+	describe('max', function() {
+
+		it('should exist', function() {
+
+			expect(Validation.testExists('max')).to.equal(true)
+
+		})
+
+		it('should pass', function() {
+
+			expect(Validation.test('max', 0, 10)).to.equal(true)
+			expect(Validation.test('max', 10, 10)).to.equal(true)
+
+		})
+
+		it('should fail', function() {
+
+			expect(Validation.test('max', 11, 10)).to.equal(false)
+			expect(Validation.test('max', 200, 10)).to.equal(false)
+
+		})
+
+	})
+
+	describe('minLen', function() {
+
+		it('should exist', function() {
+
+			expect(Validation.testExists('minLen')).to.equal(true)
+
+		})
+
+		it('should pass', function() {
+
+			expect(Validation.test('minLen', 'hi!', 3)).to.equal(true)
+			expect(Validation.test('minLen', ' ', 1)).to.equal(true)
+
+		})
+
+		it('should fail', function() {
+
+			expect(Validation.test('minLen', 'hi', 3)).to.equal(false)
+			expect(Validation.test('minLen', 'friend', 10)).to.equal(false)
+
+		})
+
+	})
+
+	describe('maxLen', function() {
+
+		it('should exist', function() {
+
+			expect(Validation.testExists('minLen')).to.equal(true)
+
+		})
+
+		it('should pass', function() {
+
+			expect(Validation.test('maxLen', 'hi', 3)).to.equal(true)
+			expect(Validation.test('maxLen', 'friend', 10)).to.equal(true)
+
+		})
+
+		it('should fail', function() {
+
+			expect(Validation.test('maxLen', 'hi!!', 3)).to.equal(false)
+			expect(Validation.test('maxLen', '  ', 1)).to.equal(false)
+
+		})
+
+	})
+
+	describe('precision', function() {
+
+		it('should exist', function() {
+
+			expect(Validation.testExists('precision')).to.equal(true)
+
+		})
+
+		it('should pass', function() {
+
+			expect(Validation.test('precision', 10, 4)).to.equal(true)
+			expect(Validation.test('precision', 0.12, 4)).to.equal(true)
+			expect(Validation.test('precision', 0.0001, 4)).to.equal(true)
+			expect(Validation.test('precision', null, 4)).to.equal(true)
+			expect(Validation.test('precision', false, 4)).to.equal(true)
+			expect(Validation.test('precision', 'sometext', 4)).to.equal(true)
+
+		})
+
+		it('should fail', function() {
+
+			expect(Validation.test('precision', 1.00001, 4)).to.equal(false)
+			expect(Validation.test('precision', 0.00003333333, 4)).to.equal(false)
+
+		})
 
 	})
 
